a61583696359d9d66a41a20483e01329a99dcdd0
GianPolicari <gianfrancopolicari@gmail.com>
2022-06-03 01:22:32 -0300

    Gian push

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
 
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/carrito', (req,res)=>{
+    res.sendFile(__dirname + '/views/productCart.html');
+});
+
+app.get('/comprar', (req,res)=>{
+    res.sendFile(__dirname + '/views/productDetail.html');
+});
+
 app.listen(420, ()=>{
     console.log("El sevido esta conectado al puerto 4:20");
      
